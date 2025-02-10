45c634c7aefd47dab6707ba14b78e63a34bcec04
Ronaldo Castillo <ronaldoc@udel.edu>
2025-02-10 01:38:25 -0500

    "Added funny joke to MainCameraController script."

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("I had a joke about variables but sadly I can’t remember it.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
